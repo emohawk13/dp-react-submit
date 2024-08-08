67b3db569bf6580740b5744b2046b0592e32ad73
emohawk13 <dauhson13@gmail.com>
2024-08-08 11:22:27 -0600

    refactored

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 set to react 17.0.2
+
+Create 2 (two) React projects:
+1 (one) React project should be made in VSCode using CRA (create-react-app)
+1 (one) React project should use codesandbox
+
+With both React projects do the following:
+Remove the rendered boilerplate code completely. This will allow you to return only the values.
+Return 2 (two) h1 tags and some text with each
+
+Additional practice: do NOT use any JSX to log the same return values as above, but in the virtual dom structure.
+
+Redo Instructions:
+Though functional components are used most often in modern react development, there are still code bases that use class based components (whether legacy or for lifecycle optimization). Please do this assignment using class-based components.
--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
-import React from "react";
+import React, { Component } from "react";
 
-function App() {
-  return React.createElement(
-    "div",
-    null,
-    React.createElement("h1", null, "Heading 1"),
-    React.createElement("p", null, "This is the first paragraph."),
-    React.createElement("h1", null, "Heading 2"),
-    React.createElement("p", null, "This is the second paragraph.")
-  );
+class App extends Component {
+  render() {
+    return React.createElement(
+      "div",
+      null,
+      React.createElement("h1", null, "Heading 1"),
+      React.createElement("p", null, "This is the first paragraph."),
+      React.createElement("h1", null, "Heading 2"),
+      React.createElement("p", null, "This is the second paragraph.")
+    );
+  }
 }
 
 export default App;
